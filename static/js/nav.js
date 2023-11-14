c10322147128eae548065fc65bb7dc6781c6cffe
Random Cosmos <srijanwhat2@gmail.com>
2023-11-14 10:00:58 +0530

    Home Page Done

--- static/js/nav.js
+++ static/js/nav.js
@@ -9,3 +9,19 @@ if ((window.location.href).indexOf('localhost') !== 7) {
         window.location.href = loc.replace('http://', 'https://');
     }
 }
+
+// hides navbar on scroll
+var prevScrollpos = 0;
+$("nav").css("top", "0");
+
+$(window).scroll(() => {
+  var currentScrollPos = window.scrollY;
+
+  if (prevScrollpos > currentScrollPos) {
+    $("nav").css("top", "0");
+  } else {
+    $("nav").css("top", "-100px");
+  }
+  
+  prevScrollpos = currentScrollPos;
+});
