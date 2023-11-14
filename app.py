754c2214c9d41d0489e59977b3e78adebe36d3c5
Random Cosmos <srijanwhat2@gmail.com>
2023-11-14 13:35:10 +0530

    search page complete

--- app.py
+++ app.py
@@ -16,5 +16,39 @@ cur = db.cursor()
 def home():
     return render_template("home.html")
 
+@app.route("/search", methods=['POST', 'GET'])
+def search():
+    cur.execute("select * from airport")
+    airports = cur.fetchall()
+    airport = []
+
+    for i in airports:
+        airport.append([i[0], i[1]])
+    
+    if request.method == "GET":
+        available="Start"
+        return render_template("search.html", airports=airport, available=available)
+    else:
+        data = request.form
+        origin = data['origin']
+        destination = data['destination']
+        cur.execute(f"select type from can_land where code='{origin}' INTERSECT select type from can_land where code='{destination}'")
+        plane = cur.fetchall()
+
+        if len(plane) > 0:
+            available = []
+            for type in plane:
+                cur.execute(f"select id, num_of_seats from airplane where type='{type[0]}' UNION select company,fare from airplane_type where type='{type[0]}'")
+                result = cur.fetchall()
+                company = result.pop()
+                for i in result:
+                    available.append([i,company])
+        else:
+            available = "Not Found"
+            
+        print(available)
+        return render_template("search.html", airports=airport, available=available)
+
+
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(debug=True, port=3000)
--- static/js/nav.js
+++ static/js/nav.js
@@ -1,15 +1,3 @@
-$('.hamburger').click(() => {
-    $('.hamburger').toggleClass('is-active');
-    $('nav div').toggleClass('show');
-});
-
-if ((window.location.href).indexOf('localhost') !== 7) {
-    let loc = window.location.href + '';
-    if (loc.indexOf('http://') === 0) {
-        window.location.href = loc.replace('http://', 'https://');
-    }
-}
-
 // hides navbar on scroll
 var prevScrollpos = 0;
 $("nav").css("top", "0");
